e64af539ef45f786971d31f3b08ec26e04e6bf55
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2019-03-19 10:54:27 +0200

    testien valitallennus

---
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.unicafe;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ossij
+ */
+public class KassapaateTest {
+    
+    Kassapaate kassapaate;
+    Maksukortti kortti;
+  
+    
+    @Before
+    public void setUp() {
+        kassapaate = new Kassapaate();
+        kortti = new Maksukortti(400);
+    }
+    
+    @Test
+    public void luotuKassapaateOlemassa() {
+        assertTrue(kassapaate!=null);      
+    }
+    
+    @Test
+    public void kassassaAluksiOikeaMaaraRahaa(){
+        assertEquals(100000,kassapaate.kassassaRahaa());
+    }
+    
+    @Test
+    public void edullisiaLounaitaEiOleMyyty(){
+        assertEquals(0, kassapaate.edullisiaLounaitaMyyty());
+    }
+    
+    @Test
+    public void myytyjenEdullistenMaaraKasvaa(){
+        kassapaate.syoEdullisesti(300);
+        
+        assertEquals(1,kassapaate.edullisiaLounaitaMyyty());
+    }
+    
+     @Test
+    public void myytyjenMaukkaidenMaaraKasvaa(){
+        kassapaate.syoMaukkaasti(400);
+        
+        assertEquals(1,kassapaate.maukkaitaLounaitaMyyty());
+    }
+    
+    
+    @Test
+    public void kateisMaksuLisaaSaldoaEdulliselleLounaalle(){
+        kassapaate.syoEdullisesti(240);
+        
+        assertEquals(100000+240,kassapaate.kassassaRahaa());
+    }
+    
+    @Test
+    public void kateisMaksuLisaaSaldoaMaukkaalleLounaalle(){
+        kassapaate.syoMaukkaasti(400);
+        
+        assertEquals(100000+400,kassapaate.kassassaRahaa());
+    }
+    
+   
+    
+    @Test
+    public void kateisMaksuPalauttaaOikeinEdulliselleLounaalle(){
+        
+        assertEquals(100, kassapaate.syoEdullisesti(340));
+    }
+    
+     @Test
+    public void kateisMaksuPalauttaaOikeinMaukkalleLounaalle(){
+        
+        assertEquals(20, kassapaate.syoMaukkaasti(420));
+    }
+    
+    
+    @Test
+    public void edullisenLounaanKorttiMaksuOttaaKortiltaRahaa(){
+        kassapaate.syoEdullisesti(kortti);
+        assertEquals(160,kortti.saldo());
+    }
+    
+    
+    
+     @Test
+    public void maukkaanLounaanKorttiMaksuOttaaRahaaKortilta(){
+        kassapaate.syoMaukkaasti(kortti);
+        assertEquals(0,kortti.saldo());
+    }
+    
+//     @Test
+//    public void kortilleRahanLataaminenOnnistuu(){
+//        
+//        assertEquals(0,kortti.saldo());
+//    }
+    
+    
+    
+}
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,90 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein(){
+        
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void positiivisenSummanLisaysOnnistuu(){
+    
+        kortti.lataaRahaa(5);
+        assertEquals(15, kortti.saldo());
+        
+}
+//    
+//     @Test
+//    public void eiVoiLisataNegatiivistaSummaa(){
+//    
+//        kortti.lataaRahaa(-5);
+//        assertEquals(10, kortti.saldo());
+//        
+//}
+    
+      @Test
+    public void rahanOttaminenToimiiJosRahaaOnTarpeeksi(){
+    
+        kortti.otaRahaa(5);
+        assertEquals(5, kortti.saldo());
+        
+}
+    
+    
+      @Test
+    public void saldoMeneeNollaanJosTasaRaha(){
+    
+        kortti.otaRahaa(5);
+        kortti.otaRahaa(5);
+        
+        assertEquals(0, kortti.saldo());
+        
+}
+    
+       @Test
+    public void saldoEiMeneMiinukselleJosRahatEiRiita(){
+    
+        kortti.otaRahaa(5);
+        kortti.otaRahaa(3);
+        kortti.otaRahaa(5);
+        assertEquals(2, kortti.saldo());
+        
+}
+    
+    @Test
+    public void rahanOttaminenPalauttaaTrueJosKortilleJaaRahaa(){
+        boolean onnistuiko = kortti.otaRahaa(5);
+        
+        
+        assertEquals(true, onnistuiko);
+    }
+    
+     @Test
+    public void rahanOttaminenPalauttaaTrueJosKortilleJaaNollaEuroa(){
+        kortti.otaRahaa(5);
+        boolean onnistuiko = kortti.otaRahaa(5);
+        
+        
+        assertEquals(true, onnistuiko);
+    }
+    
+     @Test
+    public void rahanOttaminenPalauttaaFalseJosEionnistu(){
+        kortti.otaRahaa(8);
+        boolean onnistuiko = kortti.otaRahaa(5);
+        
+        
+        assertEquals(false, onnistuiko);
+    }
+    
+    @Test
+    public void toStringPalauttaaOikein(){
+        
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    
+    
 }
